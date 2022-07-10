caa30bc8b7594054d6244a53e5490d84632bf05b
RDPolarity <rdpolarity@gmail.com>
2022-07-10 23:21:47 +1000

    Improved Tag Model

--- config/database.js
+++ config/database.js
@@ -1,20 +1,11 @@
-const parse = require('pg-connection-string').parse;
+const path = require('path');
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  
-  return {
-      connection: {
-          client: 'postgres',
-          connection: {
-              host,
-              port,
-              database,
-              user,
-              password,
-              ssl: { rejectUnauthorized: false },
-          },
-          debug: false,
-      },
-  }
-};
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
