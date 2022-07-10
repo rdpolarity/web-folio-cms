71772218963603552031579fdde8c2359cd787a3
RDPolarity <rdpolarity@gmail.com>
2022-07-10 22:44:11 +1000

    Added 3rd Party Image Provider and Vercel Env Management

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const path = require('path');
+const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  
+  return {
+      connection: {
+          client: 'postgres',
+          connection: {
+              host,
+              port,
+              database,
+              user,
+              password,
+              ssl: { rejectUnauthorized: false },
+          },
+          debug: false,
+      },
+  }
+};
--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({env}) => ({
   graphql: {
     config: {
       playgroundAlways: true,
@@ -9,5 +9,20 @@ module.exports = () => ({
         introspection: true,
       },
     }
-  }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 })
