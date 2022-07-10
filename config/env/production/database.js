63d59c6ef154eaaa7442d7f71a20fa5ead748ddb
RDPolarity <rdpolarity@gmail.com>
2022-07-10 13:49:50 +1000

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,21 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  
+  return {
+      connection: {
+          client: 'postgres',
+          connection: {
+              host,
+              port,
+              database,
+              user,
+              password,
+              ssl: { rejectUnauthorized: false },
+          },
+          debug: false,
       },
-    },
-    debug: true,
-  },
-});
+  }
+};
