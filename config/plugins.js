a5e6260220fb4dd3f8b13c38c34efe1420843ede
RDPolarity <rdpolarity@gmail.com>
2022-07-10 13:41:59 +1000

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,12 @@
 module.exports = () => ({
   graphql: {
-    enabled: true,
     config: {
       playgroundAlways: true,
       defaultLimit: 10,
       maxLimit: 20,
       apolloServer: {
         tracing: true,
+        introspection: true,
       },
     }
   }
